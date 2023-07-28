c179c8b9f1b49bb595b0840e67f76496c52fdda0
ᴍʀ.ɴɪᴋᴋɪ <MrNikki169@gmail.com>
2023-07-28 14:12:55 +0600

    Delete Abg/inline directory

--- Abg/inline/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-thanks 
-__author__ = 'PyMaster'
-"""
-
-from .inline_keyboard import InlineButton, InlineKeyboard
-from .inline_pagination_keyboard import InlineButton, InlinePaginationKeyboard
-from .reply_keyboard import ForceReply, ReplyButton, ReplyKeyboard, ReplyKeyboardRemove
